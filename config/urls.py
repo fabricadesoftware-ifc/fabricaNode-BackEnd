9cd7a155bbb56aed6ae3dfd361633cbf6e10e994
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-04-30 16:38:25 -0300

    FEAT: Implementando as rotas da api

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from fabricaNode.views import AreaViewSet, AutorViewSet, CidadeViewSet, EditoraViewSet, EstadoViewSet, KeywordViewSet, PaisViewSet, Publicacao_keywordViewSet, PublicacaoViewSet, SubareaViewSet
+
+router = DefaultRouter()
+
+router.register(r'areas', AreaViewSet)
+router.register(r'autors', AutorViewSet)
+router.register(r'cidades', CidadeViewSet)
+router.register(r'editoras', EditoraViewSet)
+router.register(r'estados', EstadoViewSet)
+router.register(r'keywords', KeywordViewSet)
+router.register(r'pais', PaisViewSet)
+router.register(r'publicacao_keywords', Publicacao_keywordViewSet)
+router.register(r'publicacaos', PublicacaoViewSet)
+router.register(r'subareas', SubareaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/", include(router.urls)),
 ]
