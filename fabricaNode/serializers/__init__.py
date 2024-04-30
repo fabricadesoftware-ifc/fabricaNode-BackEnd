4f34c98b478ec2bfcdc73bd8cd85fa9251b64c68
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-04-30 15:14:35 -0300

    FEAT: Criando serializer e views da model estado

--- fabricaNode/serializers/__init__.py
+++ fabricaNode/serializers/__init__.py
@@ -2,3 +2,4 @@ from .area import AreaSerializer
 from .autor import AutorSerializer
 from .cidade import CidadeSerializer
 from .editora import EditoraSerializer
+from .estado import EstadoSerializer
--- fabricaNode/views/__init__.py
+++ fabricaNode/views/__init__.py
@@ -2,3 +2,4 @@ from .area import AreaViewSet
 from .autor import AutorViewSet
 from .cidade import CidadeViewSet
 from .editora import EditoraViewSet
+from .estado import EstadoViewSet
