8a108e53b05ce47f0783d1dbe386722a22b1d299
isabellirosa <isabelli.ifc@gmail.com>
2025-04-29 15:46:51 -0300

    feat: Criação do png das models #51

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt",
